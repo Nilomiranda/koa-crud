e4a02fdf63b449881282dee6c92b11a3cc08299d
Nilomiranda <nilomiranda3@gmail.com>
2021-07-06 20:14:50 -0300

    install typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import "reflect-metadata";
+import Koa from 'koa'
 import dotenv from 'dotenv'
 
+const app = new Koa()
+
 dotenv.config()
+
+app.listen(process.env.APPLICATION_PORT, () => {
+  console.log(`Application running on port ${process.env.APPLICATION_PORT}`)
+})
