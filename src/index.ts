3b876a28c5de3921e3d1eeb30e6a23452ec2745d
Nilomiranda <nilomiranda3@gmail.com>
2021-07-06 21:40:25 -0300

    configured typeorm, typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import Koa from 'koa'
 import dotenv from 'dotenv'
+import { createConnection } from 'typeorm'
 
 const app = new Koa()
 
@@ -8,4 +9,10 @@ dotenv.config()
 
 app.listen(process.env.APPLICATION_PORT, () => {
   console.log(`Application running on port ${process.env.APPLICATION_PORT}`)
+})
+
+createConnection().then(() => {
+  console.log('Successfully connected to database')
+}).catch(err => {
+  console.error('Error creating database connection', err)
 })
